669c4d2ba68c2c314586bd9ffc2835cea6ef73b6
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2016-04-21 17:07:28 +0200

    polls: added another column to list_display of Questions admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -19,6 +19,6 @@ class QuestionAdmin(admin.ModelAdmin):
         }),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Question, QuestionAdmin)
